d924203165473e37b0ff7e6f7e352f6138ec9640
advait <advaitnair1111@gmail.com>
2026-04-25 23:28:33 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -0,0 +1,16 @@
+// Last updated: 4/25/2026, 11:28:33 PM
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int l = 0;
+
+        for(int r=0; r<nums.length; r++){
+            if(nums[r] != 0){
+                int temp = nums[l];
+                nums[l] = nums[r];
+                nums[r] = temp;
+                l++;
+            }
+            
+        }
+    }
+}
