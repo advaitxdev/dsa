20e8e5c28989e75bb6591d71794b350ee02a7c8c
advait <advaitnair1111@gmail.com>
2026-04-25 23:34:56 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -0,0 +1,18 @@
+// Last updated: 4/25/2026, 11:34:55 PM
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int l = 0;
+        int r = s.length()-1;
+
+        while(l<r){
+            if(s.charAt(l) != s.charAt(r)){
+                return false;
+            }
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+}
