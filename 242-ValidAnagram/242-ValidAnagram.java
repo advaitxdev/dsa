4e3d3b87e82148055632a35c34db2eae1d3c03fc
advait <advaitnair1111@gmail.com>
2026-04-25 23:34:46 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,12 @@
+// Last updated: 4/25/2026, 11:34:46 PM
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] c1 = s.toCharArray();
+        char[] c2 = t.toCharArray();
+
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+
+        return Arrays.equals(c1,c2);
+    }
+}
