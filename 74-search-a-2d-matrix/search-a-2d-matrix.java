f360d1c7392c651c33dd56453c311812ff01a4be
advait <advaitnair1111@gmail.com>
2026-09-05 22:56:45 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (24.34%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int l = 0, r = m*n-1;
+
+        while(l <= r){
+            int mid = (l+r)/2;
+            int midElement = matrix[mid/n][mid%n];
+
+            if(target == midElement){
+                return true;
+            } else if(target > midElement){
+                l = mid+1;
+            } else{
+                r = mid-1;
+            }
+        }
+
+        return false;
+    }
+}
